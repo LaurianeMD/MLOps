9c60e484368a51ab1b2dc96cc67adc2a625a16c3
Lauriane <dmbagdjelauriane@gmail.com>
2024-07-25 12:53:15 +0000

    update api.py

--- week1/3-project/api.py
+++ week1/3-project/api.py
@@ -29,38 +29,38 @@ class Customer(BaseModel):
     MonthlyCharges: float
     TotalCharges: float
 
-# @app.post('/predict')
-# def predict(customer: Customer):
-#     # Convertir les données de l'entrée en DataFrame
-#     input_data = pd.DataFrame([customer.dict()])
-    
-#     # Prédiction
-#     prediction = model.predict(input_data)
-    
-#     result = 'Churn' if prediction[0] == 1 else 'Not Churn'
-#     return {"prediction": result}
-
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
 @app.post('/predict')
 def predict(customer: Customer):
-    try:
-        # Convertir les données de l'entrée en DataFrame
-        input_data = pd.DataFrame([customer.dict()])
+    # Convertir les données de l'entrée en DataFrame
+    input_data = pd.DataFrame([customer.dict()])
+    
+    # Prédiction
+    prediction = model.predict(input_data)
+    
+    result = 'Churn' if prediction[0] == 1 else 'Not Churn'
+    return {"prediction": result}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+# @app.post('/predict')
+# def predict(customer: Customer):
+#     try:
+#         # Convertir les données de l'entrée en DataFrame
+#         input_data = pd.DataFrame([customer.dict()])
         
-        # Assurez-vous que les colonnes sont dans le même ordre que celles utilisées pour l'entraînement
-        # input_data = preprocess(input_data)  # Appliquer le prétraitement nécessaire
+#         # Assurez-vous que les colonnes sont dans le même ordre que celles utilisées pour l'entraînement
+#         # input_data = preprocess(input_data)  # Appliquer le prétraitement nécessaire
         
-        # Prédiction
-        prediction = model.predict(input_data)
+#         # Prédiction
+#         prediction = model.predict(input_data)
         
-        result = 'Churn' if prediction[0] == 1 else 'Not Churn'
-        return {"prediction": result}
-    except Exception as e:
-        return {"error": str(e)}
+#         result = 'Churn' if prediction[0] == 1 else 'Not Churn'
+#         return {"prediction": result}
+#     except Exception as e:
+#         return {"error": str(e)}
 
 
 
